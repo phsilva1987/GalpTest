cd2b507d8f34766562fa929f106dc92d39e2de76
Gustavo Antunes <gustavoifsp@hotmail.com>
2023-05-21 17:59:33 -0300

    Subindo infraestrutura

---
+++ variables.tf
@@ -0,0 +1,12 @@
+variable "AWS_access_key" {
+  description = "AWS access key."
+}
+
+variable "AWS_secret_key" {
+  description = "AWS secret key."
+}
+
+variable "AWS_region" {
+  description = "AWS region."
+  default     = "us-east-1"
+}
